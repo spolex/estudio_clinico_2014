e7d1efb5642f1120c751a24bd45f56b0a11ea8fe
Iñigo Sanchez <jisanchez003@ehu.es>
2014-08-02 17:03:02 +0200

    Prototipo identificarse

--- Osakidetza/src/pack/osakidetza/prototipos/IU_Identificarse.java
+++ Osakidetza/src/pack/osakidetza/prototipos/IU_Identificarse.java
@@ -6,10 +6,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class IU_Identificarse extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textUsuario;
+	private JPasswordField passUsuario;
 
 	/**
 	 * Launch the application.
@@ -35,8 +45,52 @@ public class IU_Identificarse extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		setResizable(false);//Para evitar que se pueda maximizar.
+		
+		JLabel lblBienvenido = new JLabel("Bienvenido al sistema");
+		lblBienvenido.setHorizontalAlignment(SwingConstants.CENTER);
+		lblBienvenido.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 22));
+		lblBienvenido.setBounds(27, 12, 409, 23);
+		contentPane.add(lblBienvenido);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.setBounds(319, 237, 117, 25);
+		contentPane.add(btnSalir);
+		
+		JLabel lblUsuario = new JLabel("Usuario");
+		lblUsuario.setBounds(27, 71, 70, 15);
+		contentPane.add(lblUsuario);
+		
+		JLabel lblContrasea = new JLabel("Contraseña");
+		lblContrasea.setBounds(27, 111, 107, 15);
+		contentPane.add(lblContrasea);
+		
+		textUsuario = new JTextField();
+		textUsuario.setBounds(133, 69, 194, 19);
+		contentPane.add(textUsuario);
+		textUsuario.setColumns(10);
+		
+		passUsuario = new JPasswordField();
+		passUsuario.setBounds(133, 111, 194, 17);
+		contentPane.add(passUsuario);
+		
+		JButton btnEntrar = new JButton("Entrar");
+		btnEntrar.setBounds(27, 160, 194, 25);
+		contentPane.add(btnEntrar);
+		
+		JButton btnCambiarPass = new JButton("Cambiar contraseña");
+		btnCambiarPass.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCambiarPass.setBounds(27, 197, 194, 25);
+		contentPane.add(btnCambiarPass);
+		
+		JButton btnRecuperarPass = new JButton("Recuperar contraseña");
+		btnRecuperarPass.setBounds(27, 237, 194, 25);
+		contentPane.add(btnRecuperarPass);
 	}
-
 }
