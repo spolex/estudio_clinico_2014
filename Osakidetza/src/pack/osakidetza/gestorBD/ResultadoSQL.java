83e71ac99af110588f881c01e7a620b079a94e1d
Iñigo Sanchez <jisanchez003@ehu.es>
2014-08-09 14:02:52 +0200

    Conector base datos

---
+++ Osakidetza/src/pack/osakidetza/gestorBD/ResultadoSQL.java
@@ -0,0 +1,138 @@
+package pack.osakidetza.gestorBD;
+
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.GregorianCalendar;
+
+public class ResultadoSQL {
+	
+	private ResultSet rdo;
+	
+	public ResultadoSQL()
+	{
+		this.rdo = null;
+	}
+	
+	public ResultadoSQL(ResultSet pRdo)
+	{
+		this.rdo = pRdo;
+	}
+	
+	public boolean next()
+	{
+		if(this.rdo != null)
+		{
+			try {
+				
+				return this.rdo.next();
+			
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error en la conexion con la base de datos");
+			}
+		}
+		//si el resultado es nulo o falla la conexion
+		//se devuelve false
+		return false;
+	}
+	
+	/**
+	 * devuelve el campo de la fila actual en la columna especificada 
+	 * como un string
+	 * @param pNomAtributo Nombre de la columna de la tabla
+	 * @return un string, null en caso de que falle la conexion
+	 * a la base de datos
+	 */
+	public String get(String pNomAtributo)
+	{
+		try {
+			
+			return this.rdo.getString(pNomAtributo);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error en la conexion con la base de datos");
+			return null;
+		}
+	}
+
+	/**
+	 * devuelve el campo de la fila actual en la columna especificada 
+	 * como un entero
+	 * @param pNomAtributo Nombre de la columna de la tabla
+	 * @return un entero, 0 en caso de que falle la conexion
+	 * a la base de datos
+	 */
+	public int getInt(String pNomAtributo)
+	{
+		try {
+			
+			return this.rdo.getInt(pNomAtributo);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error en la conexion con la base de datos");
+			return 0;
+		}
+	}
+	
+	/**
+	 * devuelve el campo de la fila actual en la columna especificada 
+	 * como un booleano
+	 * @param pNomAtributo Nombre de la columna de la tabla
+	 * @return un nooleano, false en caso de que falle la conexion
+	 * a la base de datos
+	 */
+	public boolean getBoolean(String pNomAtributo)
+	{
+		try {
+			
+			return this.rdo.getBoolean(pNomAtributo);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error en la conexion con la base de datos");
+			return false;
+		}
+	}
+	
+	
+	/**
+	 * devuelve el campo de la fila actual en la columna especificada 
+	 * como una fecha de tipo GregorianCalendar
+	 * @param pNomAtributo Nombre de la columna de la tabla
+	 * @return la fecha como GregorianCalendar, null en caso de que falle la conexion
+	 * a la base de datos
+	 */
+	public GregorianCalendar getDate(String pNomAtributo)
+	{
+		try {
+			
+			Date fecha = this.rdo.getDate(pNomAtributo);
+			//transformamos la fecha obtenida a un objeto GregorianCalendar
+			return (new GregorianCalendar((fecha.getYear()),fecha.getMonth(),fecha.getDate()));
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error en la conexion con la base de datos");
+			return null;
+		}
+	}
+	
+	/**
+	 * cierra la conexion con la base de datos
+	 */
+	public void close()
+	{
+		try {
+			
+			this.rdo.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Fallo al cerrar la conexión");
+		}
+	}
+	
+}
---
+++ Osakidetza/src/pack/osakidetza/gestorBD/SGBD.java
@@ -0,0 +1,113 @@
+package pack.osakidetza.gestorBD;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+public class SGBD {
+	
+	private static SGBD mSGDB = new SGBD();
+	
+	private SGBD()
+	{
+		
+		
+	}
+	
+	public static SGBD getSGBD()
+	{
+		return mSGDB;
+	}
+	
+	/**
+	 * devuelve la conexion con la base de datos
+	 * @return conexion con la BD
+	 */
+	@SuppressWarnings("finally")
+	private Connection getConexion()
+	{
+		Connection conexion = null;
+		 try {
+ 
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUser("liga");
+        dataSource.setPassword("x6a4Fj8q");
+        dataSource.setDatabaseName("ligabbva");
+        dataSource.setServerName("localhost");
+
+         conexion = dataSource.getConnection();
+         
+		 } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Error al conectar a la base de datos");
+			e.printStackTrace();
+		}
+		finally
+		{
+			return conexion;
+		}
+	}
+	
+	/**
+	 * ejecuta ordenes SQL que no devuelvan resultado
+	 */
+	public void execSQL(String orden)
+	{
+		Connection conexion = this.getConexion();
+		
+		if(conexion != null)
+		{
+			
+			try {
+				//creamos la sentencia
+				Statement s = conexion.createStatement();
+				//ejecutamos la orden
+				s.executeUpdate(orden);
+				//cerramos la conexion
+				conexion.close();
+			
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error al ejecutar la sentencia SQL");
+				e.printStackTrace();
+			}
+	
+		}
+	}
+	
+	/**
+	 * ejecuta ordenes SQL que devuelvan resultado
+	 * @param consulta la sentencia a ejecutar
+	 * @return Devuelve un ResultadoSQL
+	 */
+	public ResultadoSQL consultaSQL(String consulta)
+	{
+		Connection conexion = this.getConexion();
+		
+		if(conexion != null)
+		{
+			
+			try {
+				//creamos la sentencia
+				Statement s = conexion.createStatement();
+				//obtenemos su resultado
+				ResultSet rs = s.executeQuery(consulta);
+				//creamos el ResultadoSQL a devolver
+				//la conexion se cerrara al ejecutar el close del ResultSet
+				return new ResultadoSQL(rs);
+			
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Error al ejecutar la sentencia SQL");
+			}
+	
+		}
+		//si falla la conexion o la ejecucion de la sentencia,
+		//se devuelve un resultado vacio
+		return new ResultadoSQL();
+	}
+
+}
